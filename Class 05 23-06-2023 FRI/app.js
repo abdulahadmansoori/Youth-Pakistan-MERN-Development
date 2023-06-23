cc75fe86149230caa1036f559d216f2c0ad9e5e2
Abdul Ahad Mansoori <109919558+abdulahadmansoori@users.noreply.github.com>
2023-06-23 10:53:33 +0500

    Class 05 23-06-2023 FRI

---
+++ Class 05 23-06-2023 FRI/app.js
@@ -0,0 +1,94 @@
+// data types
+// premitive
+
+// string  'akkk', "asa"
+// number 23, 0.25
+// boolean true || false
+
+// non-premitive
+
+// array = ["sdsd",555,true]
+// object = {
+//     name:"",
+// }
+
+// math operations
+
+// a + b
+// a - b
+// a * b
+// a / b
+// a+=b    a=a+b
+// a-=b    a=a-b
+// ++a
+// a++ a = a + 1
+// var a = "5";
+// var b = 5;
+// console.log(a*b);
+
+// if(a==5 && a!="5"){
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// a=5;
+// a==5;
+// a===5;
+// if(a!==5){
+//     a=5
+// }
+var a = 3
+if (a > 5) {
+    console.log("a>5");
+} else if (a < 5) {
+    console.log("a<5");
+} else {
+    console.log("a=5");
+}
+
+// if(email != storeEmail){
+//     if( password == storePassword){
+
+//     }
+// }
+
+
+var marks = 93;
+if (marks >= 90) {
+    console.log("A");
+}
+else if (marks >= 80) {
+    console.log("B");
+}
+else if (marks >= 70) {
+    console.log("C");
+}
+else if (marks >= 60) {
+    console.log("D");
+}
+else {
+    console.log("F");
+}
+switch (true) {
+    case (marks >= 90): console.log("A");
+        break;
+    case (marks >= 80): console.log("B");
+        break;
+    case (marks >= 70): console.log("C");
+        break;
+    case (marks >= 60): console.log("C");
+        break;
+    case (marks >= 50): console.log("C");
+        break;
+    default: console.log('F');
+        break;
+}
+
+function grading(marks){
+    return
+
+}
+function grading(){
+
+}

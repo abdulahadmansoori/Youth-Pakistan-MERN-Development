403c35f5b125ebfe78f080939f173f22a0675155
Abdul Ahad Mansoori <109919558+abdulahadmansoori@users.noreply.github.com>
2023-06-26 12:49:36 +0500

    Class 06 26-06-2023

--- Class 05 23-06-2023 FRI/app.js
+++ Class 05 23-06-2023 FRI/app.js
@@ -1,15 +1,15 @@
 // data types
 // premitive
 
-// string  'akkk', "asa"
+// string  'akkk', "asa#512"
 // number 23, 0.25
 // boolean true || false
 
 // non-premitive
 
-// array = ["sdsd",555,true]
+// array = ["ahad",555,true]
 // object = {
-//     name:"",
+//     name:"ahad",
 // }
 
 // math operations
@@ -20,17 +20,22 @@
 // a / b
 // a+=b    a=a+b
 // a-=b    a=a-b
-// ++a
-// a++ a = a + 1
-// var a = "5";
+// ++a --a
+// a++ a-- a = a + 1
+// var a = 5;
 // var b = 5;
-// console.log(a*b);
+// console.log(a + b);
 
-// if(a==5 && a!="5"){
-//     console.log("true");
-// } else {
-//     console.log("false");
-// }
+// var firstName = "abdul"
+// var lastName = "ahad"
+// console.log(firstName + " " + lastName);
+
+var age = 18;
+if (age >= 18) {
+    console.log(" allow");
+} else {
+    console.log("not allow");
+}
 
 // a=5;
 // a==5;
@@ -38,19 +43,26 @@
 // if(a!==5){
 //     a=5
 // }
-var a = 3
+var a = 2
 if (a > 5) {
     console.log("a>5");
-} else if (a < 5) {
-    console.log("a<5");
+} else if (a > 3) {
+    console.log("a>3");
+} else if (a > 2) {
+    console.log("a>2");
 } else {
-    console.log("a=5");
+    console.log("a<=2");
 }
 
 // if(email != storeEmail){
 //     if( password == storePassword){
+//         // console.log("LOGIN")
+//     }
+//     else{
 
 //     }
+// } else{
+
 // }
 
 
@@ -70,25 +82,25 @@ else if (marks >= 60) {
 else {
     console.log("F");
 }
-switch (true) {
-    case (marks >= 90): console.log("A");
-        break;
-    case (marks >= 80): console.log("B");
-        break;
-    case (marks >= 70): console.log("C");
-        break;
-    case (marks >= 60): console.log("C");
-        break;
-    case (marks >= 50): console.log("C");
-        break;
-    default: console.log('F');
-        break;
-}
+// switch (true) {
+//     case (marks >= 90): console.log("A");
+//         break;
+//     case (marks >= 80): console.log("B");
+//         break;
+//     case (marks >= 70): console.log("C");
+//         break;
+//     case (marks >= 60): console.log("C");
+//         break;
+//     case (marks >= 50): console.log("C");
+//         break;
+//     default: console.log('F');
+//         break;
+// }
 
-function grading(marks){
-    return
+// function grading(marks){
+//     return
 
-}
-function grading(){
+// }
+// function grading(){
 
-}
+// }
---
+++ Class 06 26-06-2023 MON/app.js
@@ -0,0 +1,71 @@
+// marks = 92;
+// switch (true) {
+//     case (marks >= 90): {
+//         console.log("A");
+//         break;
+//     }
+//     case (marks >= 80): {
+//         console.log("B");
+//         break
+//     }
+// }
+
+
+
+
+// function Sum (a, b){
+//     var result = a+b;
+//     // console.log(document.getElementById("output").innerHTML = result);
+// console.log(result);
+//     return result;
+// }
+// Sum(5,9);
+// console.log(Sum(2,6));
+
+// function findEvenOdd(a) {
+//     if (a % 2 == 0){
+//         console.log(a+" is even")
+//     }else{
+//         console.log(a+" is odd")
+//     }
+// }
+// findEvenOdd(5);
+// findEvenOdd(2);
+// findEvenOdd(17);
+// findEvenOdd(19);
+// findEvenOdd(23);
+// findEvenOdd(7);
+
+// loops
+
+// for loop
+// var i;
+// for(var i=0; i<=10; i++){
+//     console.log(i);
+// }
+
+// while loop
+// var i =0;
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// do while loop
+// var i = 0;
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=10);
+
+// array
+// var arr = ["a","b","c","d"];
+// for(var i = 0; i<=(arr.length-1); i++){
+//     console.log(arr[i])
+// }
+
+var arr = ['a', 5, true, ['a', 'ac'], { name: "ahad" }]
+
+// foreach
+// forin
+// forout

6b95d69e8c80327edb090e81679668feebc65e86
Abdul Ahad Mansoori <109919558+abdulahadmansoori@users.noreply.github.com>
2023-07-12 10:09:38 +0500

    Class 07 B4 11-07-2023

---
+++ Class 07 B4 11-07-2023 TUE/app.js
@@ -0,0 +1,54 @@
+console.log("hello, world!!");
+var a = 5;
+
+// if (a == "5") {
+//     console.log("a is equal to 5");
+// } else {
+//     console.log("a is not equal to 5");
+// }
+
+// var age = 109;
+// if ((age >= 18) && (age <= 100)) {
+//     console.log("allow to vote");
+// } else {
+//     console.log("not allow to vote");
+// }
+// var email = "abc";
+// var password = "bca";
+// if ((email === "abc") && (password === "bca")) {
+//     console.log("allow to login");
+// } else {
+//     console.log("not allow to login");
+// }
+
+
+//  var marks = 99;
+// if (marks >= 90) {
+//     console.log("A");
+// } else if (marks >= 80) {
+//     console.log("B");
+    
+// } else if (marks >= 70) {
+//     console.log("C");
+    
+// } else if (marks >= 60) {
+//     console.log("D");
+    
+// } else {
+//     console.log("F");
+// }
+
+var amount = 200;
+
+if(amount >= 5000){
+    console.log("2%");
+}
+else if(amount >= 3000){
+    console.log("3%");
+}
+else if(amount >= 1000){
+    console.log("5%");
+} 
+else{
+    console.log("10%");
+}
